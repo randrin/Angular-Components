88469a09754b5cfa2cec58fecd7b51d736d807d9
Randrin <randrin.nzeukang@accenture.com>
2020-11-07 23:14:55 +0100

    Update nbp-dividing-line component

---
+++ src/assets/utils/nbp-commons/nbp-commons.enums.ts
@@ -0,0 +1,147 @@
+/**
+ * Enum per la definizione del colore dei componenti della libreria
+ * PRIMARY - Green
+ * SECONDARY - White
+ * THIRD - Blue
+ * DEFAULT - Grey
+ * OUTLINE - Background transparent
+ * LEFT - Background + Float left
+ * RIGHT - Background + Float right
+ */
+export enum NbpStyle {
+  PRIMARY = 'PRIMARY',
+  DANGER = 'DANGER',
+  SECONDARY = 1,
+  THIRD = 2,
+  FOURTH = 3,
+  FIFTH = 4,
+  DEFAULT = 'DEFAULT',
+  FILLED = 6,
+  OUTLINE = 7,
+  ROUNDED = 8,
+  LEFT = 9,
+  RIGHT = 10,
+  CENTER = 11,
+  JUSTIFIED = 12,
+  CHECK = 13,
+  CIRCLE = 14,
+  BACKGROUNDGRAY = 15,
+  BACKGROUNDGREEN = 16,
+  BACKGROUNDBLACK = 17,
+  PROFILE = 18
+}
+/**
+* Enum per la definizione della dimensione dei componenti della libreria
+* LG - Large
+* MD - Medium
+*/
+export declare enum NbpSize {
+  SM = 0,
+  LG = 1,
+  MD = 2
+}
+/**
+* Enum per la definizione della grandezza del font da utilizzare
+*/
+export declare enum NbpFontSize {
+  SMALL = 0,
+  MEDIUM = 1,
+  LARGE = 2
+}
+export declare enum NbpPipe {
+  CURRENCY = 0,
+  DATE = 1,
+  DECIMAL = 2,
+  LOWERCASE = 3,
+  PERCENT = 4,
+  UPPERCASE = 5,
+  CONTO = 6
+}
+/**
+* Enum per la definizione del canale applicativo
+*/
+export declare enum NbpChannel {
+  ABC = 0,
+  INTRANET = 1,
+  IB = 2
+}
+export declare enum NbpLoadingStatus {
+  START = 0,
+  STOP = 1
+}
+export declare enum NbpClientType {
+  PG = 0,
+  PF = 1
+}
+export declare enum NbpAlertType {
+  GENERIC = 0,
+  POSITIVE = 1,
+  PROMOTIONAL = 2,
+  WARNING = 3,
+  ERROR = 4
+}
+export declare enum NbpSwitchType {
+  ICON = 0,
+  TEXT = 1,
+  SMALLTEXT = 2,
+  TEXTICON = 3,
+  SMALLTEXTICON = 4
+}
+export declare enum NbpStepDimension {
+  SMALL = 1,
+  MEDIUM = 2,
+  LARGE = 3
+}
+/**
+* Enum dei tipi di Eventi/Messaggi gestiti per NDCE
+*/
+export declare enum NbpEmbeddingMessageType {
+  CJ_READY = 1,
+  CJ_STEP = 2,
+  ERROR = 3,
+  FLOATING_MENU_ON = 4,
+  FLOATING_MENU_OFF = 5,
+  GOTO = 6,
+  LOADING_WINDOW_START = 7,
+  LOADING_WINDOW_STOP = 8,
+  MODAL_WINDOW_SHOW = 9,
+  MODAL_WINDOW_HIDE = 10,
+  PAGE_SCROLL = 11,
+  PAGE_SIZE = 12,
+  SET_CONTRAST_OFF = 13,
+  SET_CONTRAST_ON = 14,
+  SET_FONT_SIZE = 15
+}
+/**
+* enum descrizioni firma
+*/
+export declare enum SignDescription {
+  ALTRO = 0,
+  ALTRO_REMOTA = 1,
+  CARTACEA = 2,
+  DIGITALE_REMOTA = 3,
+  GRAFOMETRICA = 4
+}
+/**
+* Elenco Sign Type censiti
+*/
+export declare enum SignType {
+  CARTA = 2,
+  FEA = 1,
+  FDR = 0,
+  FEA_RIATTIVAZIONE = 3
+}
+export declare enum NbpTabBarType {
+  DEFAULT = 0,
+  ICON = 1,
+  ICON_WITH_BG = 2
+}
+export declare enum NbpDividinLineColor {
+  DEFAULT = 0,
+  GRAY = 1
+}
+export declare enum NbpAccordionType {
+  DEFAULT = 0,
+  OUTER = 1,
+  INNER = 2
+}
