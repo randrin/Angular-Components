7b8ef76207ada5f9366738dc8e90fbe8033f4fb1
Randrin <randrin.nzeukang@accenture.com>
2020-11-14 22:42:54 +0100

    NbpAlertBoxComponent click close button

--- src/assets/js/nbp-scripts.js
+++ src/assets/js/nbp-scripts.js
@@ -12,3 +12,11 @@ $(document).ready(function () {
     return false;
   });
 });
+
+// Close NbpAlertBoxComponent
+$(document).ready(function () {
+  var $nbpAlertBoxWrapper = $('.nbp-alert-box-wrapper'); 
+  $(".nbp-alert-box-icon-close").click(function () {
+    $nbpAlertBoxWrapper.addClass('nbp-display-none');
+  });
+});
