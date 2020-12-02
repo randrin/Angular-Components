965c1e3bf78ac5a643790ef876f920ef1b29c5ab
Randrin <randrin.nzeukang@accenture.com>
2020-12-02 23:32:44 +0100

    First commit NbpTextareaComponent

---
+++ src/app/components/nbp-textarea/nbp-textarea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nbp-textarea',
+  templateUrl: './nbp-textarea.component.html',
+  styleUrls: ['./nbp-textarea.component.scss']
+})
+export class NbpTextareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
