b84b3d537a4fbb041ce8292b11947e7ff64f6cd8
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-01-10 12:53:20 +0100

    NbpColorComponent fixed

--- src/app/components/nbp-color/nbp-color.component.ts
+++ src/app/components/nbp-color/nbp-color.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NbpBorderClasse } from 'src/assets/utils/nbp-commons/nbp-commons.classes';
+import { NbpBackgroundClasse, NbpBorderClasse } from 'src/assets/utils/nbp-commons/nbp-commons.classes';
 import { NbpStyle } from 'src/assets/utils/nbp-commons/nbp-commons.enums';
 
 @Component({
@@ -15,8 +15,11 @@ export class NbpColorComponent implements OnInit {
 
   _style = NbpStyle;
   _borderClasse = NbpBorderClasse;
+  _backgroundClasse = NbpBackgroundClasse;
 
   nbpColor: string;
+  nbpBackground: string;
+  nbpBorder: string;
   nbpSeparator: string = ' ';
 
   constructor() { }
@@ -27,10 +30,59 @@ export class NbpColorComponent implements OnInit {
 
   // Functions
   nbpSetUpComponent() {
-    this.nbpColor = this.nbpGetColorStyle(this.nbpColorStyle);
+    this.nbpBorder = this.nbpGetBorderClasse(this.nbpColorStyle);
+    this.nbpBackground = this.nbpGetBackgroundClasse(this.nbpColorStyle);
+    this.nbpColor = this.nbpBorder + this.nbpSeparator + this.nbpBackground ;
   }
 
-  nbpGetColorStyle(nbpInput) {
+  nbpGetBackgroundClasse(nbpInput) {
+    switch (nbpInput) {
+      case this._style.DEFAULT:
+        return this._backgroundClasse.DEFAULT;
+        break;
+      case this._style.WHITE:
+        return this._backgroundClasse.WHITE;
+        break;
+      case this._style.WHITE_SMOKE:
+        return this._backgroundClasse.WHITE_SMOKE;
+        break;
+      case this._style.WARM_GRAY_20:
+        return this._backgroundClasse.WARM_GRAY_20;
+        break;
+      case this._style.NIGHT_RIDER:
+        return this._backgroundClasse.NIGHT_RIDER;
+        break;
+      case this._style.SLIVER:
+        return this._backgroundClasse.SLIVER;
+        break;
+      case this._style.BLACK:
+        return this._backgroundClasse.BLACK;
+        break;
+      case this._style.PRIMARY:
+        return this._backgroundClasse.PRIMARY;
+        break;
+      case this._style.TOMATO:
+        return this._backgroundClasse.TOMATO;
+        break;
+      case this._style.INFO:
+        return this._backgroundClasse.INFO;
+        break;
+      case this._style.SUCCESS:
+        return this._backgroundClasse.SUCCESS;
+        break;
+      case this._style.WARNING:
+        return this._backgroundClasse.WARNING;
+        break;
+      case this._style.DANGER:
+        return this._backgroundClasse.DANGER;
+        break;
+      default:
+        return this._backgroundClasse.DEFAULT;
+        break;
+    }
+  }
+
+  nbpGetBorderClasse(nbpInput) {
     switch (nbpInput) {
       case this._style.DEFAULT:
         return this._borderClasse.DEFAULT;
