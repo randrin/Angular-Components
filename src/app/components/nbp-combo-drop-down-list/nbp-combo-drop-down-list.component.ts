12b36956d054acb526fe8ae37e758b927c133f27
Rodrigue Djomou Longang <rodrigue.djomou@linksmt.it>
2021-02-02 17:59:40 +0100

    Fix(creazione nuovi componenet in corso)

---
+++ src/app/components/nbp-combo-drop-down-list/nbp-combo-drop-down-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nbp-combo-drop-down-list',
+  templateUrl: './nbp-combo-drop-down-list.component.html',
+  styleUrls: ['./nbp-combo-drop-down-list.component.scss']
+})
+export class NbpComboDropDownListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/nbp-tabar/nbp-tabar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nbp-tabar',
+  templateUrl: './nbp-tabar.component.html',
+  styleUrls: ['./nbp-tabar.component.scss']
+})
+export class NbpTabarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
