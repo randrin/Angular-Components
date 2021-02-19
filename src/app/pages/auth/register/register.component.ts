ca3c7b4077ec3b45a4449e62c9d42ae0c9d28941
France84 <rodrigue.djomou@linksmt.it>
2021-02-19 20:34:14 +0100

    fix(check correct email validate)

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -13,6 +13,7 @@ export class RegisterComponent extends NbpBaseComponent implements OnInit {
   nbpRegisterDisabled: boolean = true;
   nbpRegisterErrorMessage: string = "";
   nbpConfirmPassword: string = "";
+  nbpEmailInputModel:any;
   nbpRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(injector: Injector, private nbpAuthService: NbpAuthService) {
@@ -26,9 +27,9 @@ export class RegisterComponent extends NbpBaseComponent implements OnInit {
   nbpSetUpComponent() {
     this.nbpRegisterDisabled =
       !this.nbpAuth.register.userName.length ||
-      !this.nbpAuth.register.password.length ||
-      !this.nbpAuth.register.email.length ||
-      !this.nbpConfirmPassword
+        !this.nbpAuth.register.password.length ||
+        !this.nbpAuth.register.email.length ||
+        !this.nbpConfirmPassword
         ? true
         : false;
     this.nbpRegisterErrorMessage = "";
@@ -55,6 +56,16 @@ export class RegisterComponent extends NbpBaseComponent implements OnInit {
     this.router.navigateByUrl("/login");
   }
 
+  checkCorrectEmail(input) {
+    if (this.nbpRegex.test(input)) {
+      return ''
+    }
+    else {
+      return  this.nbpRegisterErrorMessage = "Email Address format isn't correct.";
+    }
+
+  }
+
   nbpRegisterSubmit() {
     if (this.nbpAuth.register.password !== this.nbpConfirmPassword) {
       this.nbpRegisterSuccess = false;
@@ -62,10 +73,10 @@ export class RegisterComponent extends NbpBaseComponent implements OnInit {
         "Password and Confirm Password don't mactch.";
     } else {
       if (
-        this.nbpRegex.test(String(this.nbpAuth.register.email).toLowerCase())
+       ( this.nbpRegex.test(String(this.nbpAuth.register.email).toLowerCase()) || 
+        this.checkCorrectEmail(this.nbpAuth.register.email))
       ) {
         this.nbpRegisterSuccess = false;
-        this.nbpRegisterErrorMessage = "Email Address format isn't correct.";
       } else {
         this.nbpAuthService.NbpRegisterService(this.nbpAuth.register).subscribe(
           (response: any) => {
